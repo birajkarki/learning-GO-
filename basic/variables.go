aef085f5e115e65b6e1b062985e848f23a0c567f
Biraj <birajkarki9849@gmail.com>
2022-12-28 17:09:59 +0545

    commit

--- basic/variables.go
+++ basic/variables.go
@@ -5,4 +5,16 @@ import "fmt"
 func main() {
 	var a = "biraj"
 	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "apple"
+	fmt.Println(f)
 }
