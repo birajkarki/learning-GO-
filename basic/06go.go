129e744ab2be1f433b79e08070fa1f466d82ad4d
Biraj <birajkarki9849@gmail.com>
2022-12-31 23:35:11 +0545

    D

--- basic/06go.go
+++ basic/06go.go
@@ -36,3 +36,47 @@ func main() {
 	fmt.Println(true || false)
 	fmt.Println(!true)
 }
+package main
+
+import "fmt"
+
+func main() {
+	var a = "birajkarki"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "apple"
+	fmt.Println(f)
+}
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const s string = "biraj"
+
+func main() {
+	fmt.Println(s)
+
+	const n = 500000000
+
+	const d = 3e20 / n
+	fmt.Println(d)
+
+	fmt.Println(int64(d))
+
+	fmt.Println(int43(d))
+
+	fmt.Println(math.Sin(n))
+
+}
