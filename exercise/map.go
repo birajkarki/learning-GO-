ec58e05ed7ac94a75330e2d6635d156627fd4f89
Biraj <birajkarki9849@gmail.com>
2023-01-06 19:46:13 +0545

    commit

--- exercise/map.go
+++ exercise/map.go
@@ -3,6 +3,20 @@ package main
 import "fmt"
 
 func main() {
+	var (
+		phones map[string]string
+
+		// Key        : Product ID
+		// Element    : Available / Unavailable
+		products map[int]bool
+
+		multiPhones map[string][]string
+		// Key        : Last name
+		// Element    : Phone numbers
+
+		basket map[int]map[int]int
+		// Key        : Customer ID
+		// Element Key:
 		//   Key: Product ID Element: Quantity
 	)
 
